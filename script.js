46615c5bc36583c63f0bc90a92048a29b831c0ef
Adornadowilliam2 <145187649+Adornadowilliam2@users.noreply.github.com>
2024-02-18 15:32:29 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,145 @@
+function search() {
+    var input = document.querySelector('.search-input').value;
+    if (input.includes('Google')) {
+        window.location.href = 'https://www.google.com';
+    } else if (input.includes('Youtube') || input.includes('youtube') || input.includes('youtube.com')) {
+        window.location.href = 'https://www.youtube.com';
+    } else if (input.includes('Andylyn') || input.includes('andylyn')) {
+        window.location.href = 'https://www.facebook.com/Jaeeee.yuunn';
+    } else if (input.includes('Pauline') || input.includes('Lucre') ||  input.includes('pauline')) {
+        window.location.href = 'https://www.facebook.com/paulinerain.lucre.5';
+    } else if (input.includes('Sir Din') || input.includes('Aladin') ||  input.includes('best teacher') || input.includes('Sir Aladin Silvestre')||input.includes('Sir Din Silvestre')) {
+        window.location.href = 'https://www.facebook.com/aladin.silvestre.129';
+    } else if (input.includes('Onza') ||  input.includes('John Kirt')||  input.includes('john kirt') ||  input.includes('John kirt michael')) {
+        window.location.href = 'https://www.facebook.com/johnmichael.onza';
+    } else if (input.includes('Jay') ||  input.includes('jay')||  input.includes('Magoliman')) {
+        window.location.href = 'https://www.facebook.com/profile.php?id=100094166333693';
+    } else if (input.includes('Edge')) {
+        window.location.href = 'https://edge.mfi.org.ph/edgev4/my/';
+    } else if (input.includes('Lance') ||  input.includes('lance')||  input.includes('Dumago')) {
+        window.location.href = 'https://www.facebook.com/lanceemanuele.dumago';
+    } else if (input.includes('Jannine') ||  input.includes('jannine')) {
+        window.location.href = 'https://www.facebook.com/jenin.olarte';
+    } else if (input.includes('Maryjoy') ||  input.includes('maryjoy')) {
+        window.location.href = 'https://www.facebook.com/maryjoy.bernabe.562';
+    } else if (input.includes('Karla') ||  input.includes('karla')) {
+        window.location.href = 'https://www.facebook.com/karlamae.mojedo.9';
+    } else if (input.includes('Althea') ||  input.includes('althea')) {
+        window.location.href = 'https://www.facebook.com/althea.mojedo26'; 
+    } else if (input.includes('Tyrone') ||  input.includes('tangco') ||  input.includes('tj')) {
+        window.location.href = 'https://www.facebook.com/tyronejames.tangco'; 
+    } else if (input.includes('William')||  input.includes('Boss will')||  input.includes('will')) {
+        window.location.href = 'https://www.facebook.com/william.boloferadornado'; 
+    } else if (input.includes('Kissasian')) {
+        window.location.href = 'https://kissasian.lu/'; 
+    } else if (input.includes('My demon')) {
+        window.location.href = 'https://kissasian.cam/series/my-demon/'; 
+    } else if (input.includes('Intagram mp4')) {
+        window.location.href = 'https://allinonedownloader.com/instagram-video-downloader.php'; 
+    } else if (input.includes('Youtube mp4')) {
+        window.location.href = 'https://mp3-convert.org/youtube-to-mp3-converters/';
+    } else if (input.includes('Jude Africa') ||  input.includes('Christian Jude')||  input.includes('Christian Jude Africa')) {
+        window.location.href = 'https://web.facebook.com/africa.kurt';
+    } else if (input.includes('Sarah Jane') ||  input.includes('Anqui Sarah')||  input.includes('Ate Sarah')) {
+        window.location.href = 'https://web.facebook.com/janesarah.anqui';
+    } else if (input.includes('Sean') ||  input.includes('Sean Adrian')||  input.includes('Astillero')) {
+        window.location.href = 'https://web.facebook.com/search/top?q=sean%20adrian';
+    } else if (input.includes('Balbin') ||  input.includes('Jeth Balbin')||  input.includes('Jeth Balbin Lyka')) {
+        window.location.href = 'https://web.facebook.com/search/top?q=sean%20adrian';
+    } else if (input.includes('Balbin') ||  input.includes('Jeth Balbin')||  input.includes('Jeth Balbin Lyka')) {
+        window.location.href = 'https://web.facebook.com/profile.php?id=100092674045578';
+    } else if (input.includes('Rhogill Banez') ||  input.includes('Rhogill')||  input.includes('Banez')) {
+        window.location.href = 'https://web.facebook.com/search/top?q=rhogill%20ba%C3%B1ez';
+    } else if (input.includes('Charles') ||  input.includes('Charles Bendanillo')||  input.includes('Charles Miguel')) {
+        window.location.href = 'https://web.facebook.com/miguel.bendanillo';
+    } else if (input.includes('Michael Bolivar') ||  input.includes('Bolivar')||  input.includes('Torayno')) {
+        window.location.href = 'https://web.facebook.com/Kel23';
+    } else if (input.includes('Ronnel') ||  input.includes('Ronnel Caderao')||  input.includes('Caderao')) {
+        window.location.href = 'https://web.facebook.com/profile.php?id=100069722855902';
+    } else if (input.includes('James Ivan Conocono') ||  input.includes('James Ivan')||  input.includes('Conocono')) {
+        window.location.href = 'https://web.facebook.com/MonkeCry';
+    } else if (input.includes('Cortez') ||  input.includes('Jade Cortez')||  input.includes('Catherine Jade')) {
+        window.location.href = 'https://web.facebook.com/ideyc';
+    } else if (input.includes('Henry') ||  input.includes('Henry Del Monte')||  input.includes('Del Monte')) {
+        window.location.href = 'https://web.facebook.com/henrylaurence.delmonte';
+    } else if (input.includes('Grace') ||  input.includes('Grace Dinglasan')||  input.includes('Grace Ann')) {
+        window.location.href = 'https://web.facebook.com/graceann.dinglasan29';
+    } else if (input.includes('Janrick') ||  input.includes('Janrick Durana')||  input.includes('Janrick Raye')) {
+        window.location.href = 'https://web.facebook.com/Janrick.X';
+    } else if (input.includes('Shane') ||  input.includes('Shaine')||  input.includes('Shaine Collins')) {
+        window.location.href = 'https://web.facebook.com/search/top?q=shane%20collin%20s.%20egagamao';
+    } else if (input.includes('Christian Jay') ||  input.includes('Christian')||  input.includes('Encia')) {
+        window.location.href = 'https://web.facebook.com/search/top?q=shane%20collin%20s.%20egagamao';
+    } else if (input.includes('John Aaron') ||  input.includes('John Estonanto')||  input.includes('Aaron Estonanto')) {
+        window.location.href = 'https://web.facebook.com/johnaaron.estonanto.9';
+    } else if (input.includes('John Eugenio') ||  input.includes('John Guiller')||  input.includes('Guiller')) {
+        window.location.href = 'https://web.facebook.com/profile.php?id=100091899470238';
+    } else if (input.includes('Garcia Diego') ||  input.includes('Diego Garcia')||  input.includes('Igi')) {
+        window.location.href = 'https://web.facebook.com/noctile22';
+    } else if (input.includes('Rachel') ||  input.includes('Rachel Garcia')||  input.includes('Joie')) {
+        window.location.href = 'https://web.facebook.com/search/top?q=r%20joie%20garcia';
+    } else if (input.includes('Rosvei') ||  input.includes('Rosvei Hila')||  input.includes('Rosvei Hila Asani')) {
+        window.location.href = 'https://web.facebook.com/rosvey.santos';
+    } else if (input.includes('Boy tapang') ||  input.includes('Karl Arjae')||  input.includes('Karl Arjae Pacinos')) {
+        window.location.href = 'https://web.facebook.com/profile.php?id=100004934327177';
+    } else if (input.includes('Michael Paul') ||  input.includes('Para')||  input.includes('Michael Para')) {
+        window.location.href = 'https://web.facebook.com/michaelpaul.para.14';
+    } else if (input.includes('Tanya') ||  input.includes('Pontejos')||  input.includes('Tanya Casandra')) {
+        window.location.href = 'https://web.facebook.com/search/top?q=tanya%20casandra';
+    } else if (input.includes('Michael Paul') ||  input.includes('Para')||  input.includes('Michael Para')) {
+        window.location.href = 'https://web.facebook.com/michaelpaul.para.14';
+    } else if (input.includes('Tanya') ||  input.includes('Pontejos')||  input.includes('Tanya Casandra')) {
+        window.location.href = 'https://web.facebook.com/search/top?q=tanya%20casandra';
+    } else if (input.includes('Sir Romm James Cuya') ||  input.includes('Sir Romm')||  input.includes('Sir Rom James')) {
+        window.location.href = 'https://www.facebook.com/rommjames.cuya';
+    } else if (input.includes('Sir Diego Conception') ||  input.includes('Sir Jeco Conception')||  input.includes('Sir Jeco')) {
+        window.location.href = 'https://discord.com/channels/@me/1201519792495001660';
+    } else if (input.includes('Sir Mark Elrey') ||  input.includes('Sir ReyMarkus')||  input.includes('Sir Mark Elrey De Vera')) {
+        window.location.href = 'https://discord.com/channels/@me/1201520138952908880';
+    } else {
+        // Redirect to Google search
+        window.location.href = 'https://www.google.com/search?q=' + encodeURIComponent(input);
+    }
+}
+
+if ('SpeechRecognition' in window || 'webkitSpeechRecognition' in window) {
+    // Create a new instance of the SpeechRecognition object
+    const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
+
+    // Set the parameters
+    recognition.lang = 'en-US'; // Language of the speech recognition
+    recognition.continuous = true; // Keep listening for speech until stopped
+
+    // Event handler for when speech recognition starts
+    recognition.onstart = function() {
+      console.log('Speech recognition started');
+    };
+
+    // Event handler for when speech recognition results are available
+    recognition.onresult = function(event) {
+      const result = event.results[event.results.length - 1][0].transcript;
+      document.getElementById('searchInput').value = result;
+      document.getElementById('searchButton').disabled = false;
+    };
+
+    // Event handler for errors that occur during speech recognition
+    recognition.onerror = function(event) {
+      console.error('Speech recognition error', event.error);
+    };
+
+    // Event handler for when speech recognition ends
+    recognition.onend = function() {
+      console.log('Speech recognition ended');
+      document.getElementById('searchButton').disabled = true;
+    };
+
+    // Start the speech recognition when the Start button is clicked
+    document.getElementById('startButton').addEventListener('click', function() {
+      recognition.start();
+    });
+  } else {
+    // The browser doesn't support speech recognition
+    console.error('Speech recognition not supported');
+    document.getElementById('searchInput').value = 'Speech recognition not supported';
+    document.getElementById('searchButton').disabled = true;
+  }
